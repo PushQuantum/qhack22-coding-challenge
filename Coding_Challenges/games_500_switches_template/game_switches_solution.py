7b58993c16476f54dcc7d22a09cbd9a0e17fdd50
Charlotte Cullip <charlotte.cullip@gmail.com>
2022-02-17 16:03:41 +0100

    add games500 solution

---
+++ Coding_Challenges/games_500_switches_template/game_switches_solution.py
@@ -0,0 +1,67 @@
+# ! /usr/bin/python3
+
+import sys
+from pennylane import numpy as np
+import pennylane as qml
+
+
+def switch(oracle):
+    """Function that, given an oracle, returns a list of switches that work by executing a
+    single circuit with a single shot. The code you write for this challenge should be completely
+    contained within this function between the # QHACK # comment markers.
+    Args:
+        - oracle (function): oracle that simulates the behavior of the lights.
+    Returns:
+        - (list(int)): List with the switches that work. Example: [0,2].
+    """
+
+    dev = qml.device("default.qubit", wires=[0, 1, 2, "light"], shots=1)
+
+    @qml.qnode(dev)
+    def circuit():
+        # QHACK #
+
+        # You are allowed to place operations before and after the oracle without any problem.
+        # initialize first three qubits in |+> state
+        for i in range(3):
+            qml.Hadamard(wires=[i])
+        # initialize light qubit in |-> state
+        qml.PauliX(wires="light")
+        qml.Hadamard(wires="light")
+
+        oracle()
+
+        # undo Hadamard to return to computational basis
+        for i in range(3):
+           qml.Hadamard(wires=[i])
+
+        # QHACK #
+
+        return qml.sample(wires=range(3))
+
+    sample = circuit()
+
+    # QHACK #
+    # Process the received sample and return the requested list.
+    # if qubit == 1 in computational basis, light is "on"
+    list = []
+    for i in range(3):
+        if sample[i] == 1:
+            list.append(i)
+
+    return list
+
+    # QHACK #
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    numbers = [int(i) for i in inputs]
+
+    def oracle():
+        for i in numbers:
+            qml.CNOT(wires=[i, "light"])
+
+    output = switch(oracle)
+    print(*output, sep=",")
